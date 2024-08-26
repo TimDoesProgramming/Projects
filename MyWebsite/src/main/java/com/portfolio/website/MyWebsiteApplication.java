fc661fa5ce4f5e09a9502669c44fcfc97c99ac7c
Timothy A <albantimothy@gmail.com>
2024-08-26 00:37:45 -0400

    adding the queueing feature

--- MyWebsite/src/main/java/com/portfolio/website/MyWebsiteApplication.java
+++ MyWebsite/src/main/java/com/portfolio/website/MyWebsiteApplication.java
@@ -3,12 +3,15 @@ package com.portfolio.website;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MyWebsiteApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(MyWebsiteApplication.class, args);
+
 	}
 
 }
---
+++ MyWebsite/src/main/java/com/portfolio/website/configs/AsyncConfig.java
@@ -0,0 +1,21 @@
+package com.portfolio.website.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "MyExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.setThreadNamePrefix("MyExecutor-");
+        executor.initialize();
+        return executor;
+    }
+}
